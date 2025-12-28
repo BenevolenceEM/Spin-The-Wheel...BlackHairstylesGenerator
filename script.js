024d7af06285f5fbf46870cffdf1802f554120ac
BenevolenceEM <maliksofia58@gmail.com>
2025-12-27 23:46:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+const hairstyles = [
+    {name: "Afro", length: "short", style: "nat", color: "black"}
+];
 
-
+document.getElementbyId("spinBtn").addEventListener("click", () => {
+    const selectedLength = document.getElementbyId("length").value;
+    const selectedStyle = document.getElementbyId("style").value;
+    const selectedColor = document.getElementbyId("color").value;
+    
+    const filtered = hairstyles.filter(h => h.length === selectedLength && h.style === selectedStyle && h.color === selectedColor);
+    
+    if (filtered.length === 0) {
+        document.getElementbyId("result").textContent = "No matches found. Please try again...";
+        return;
+    }
+    
+    const randomIndex = Math.floor(Math.random() * filtered.length);
+    const chosenHairstyle = filtered[randomIndex];
+    
+    document.getElementById("result").textContent = 'Your next hairstyle is: ${chosenHairstyle.name}';
+});

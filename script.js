32c91725e0ed6dfd768d156bd1013941cebd866e
BenevolenceEM <maliksofia58@gmail.com>
2025-12-28 17:30:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const hairstyles = [
-    {name: "Afro", length: "short", style: "nat", color: "black"}
+    {name: "Afro", length: "short", style: "nat", color: "black"},
+    {name: "Bantu Knots", length: "short", style: "nat", color: "black"},
+    {name: "Big Chop/Pixie Cut", length: "short", style: "nat", color: "black"},
+    {name: "Blow Out/Silk Press", length: "med", style: "nat", color: "black"},
+    {name: "Boho Braids", length: "short", style: "ext", color: "black"},
+    {name: "Boho Twists", length: "short", style: "ext", color: "black"},
+    {name: "Braid Out/Twist Out", length: "short", style: "nat", color: "black"},
+    {name: "Braids with beads", length: "short", style: "ext", color: "black"},
+    {name: "Cornrows", length: "short", style: "ext", color: "black"},
+    {name: "Cornrows with beads", length: "short", style: "ext", color: "black"},
+    {name: "Faux Locs", length: "short", style: "ext", color: "black"},
+    {name: "French Curls", length: "short", style: "ext", color: "black"},
+    {name: "Knotless Braids", length: "short", style: "ext", color: "black"},
+    {name: "Leave Out/Sew Ins", length: "short", style: "ext", color: "black"},
+    {name: "Natural Braids", length: "short", style: "nat", color: "black"},
+    {name: "Natural Braids with beads", length: "short", style: "nat", color: "black"},
+    {name: "Natural Cornrows", length: "short", style: "nat", color: "black"},
+    {name: "Natural Cornrows with beads", length: "short", style: "nat", color: "black"},
+    {name: "Natural Locs", length: "short", style: "nat", color: "black"},
+    {name: "Natural Twists", length: "short", style: "nat", color: "black"},
+    {name: "Wigs", length: "short", style: "ext", color: "black"},
+    {name: "Twists", length: "short", style: "ext", color: "black"}
 ];
 
 document.getElementbyId("spinBtn").addEventListener("click", () => {
